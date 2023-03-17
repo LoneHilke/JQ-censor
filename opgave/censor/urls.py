95f8f4b4e9198679c8161f94e7a801942f7567f9
LMH <90649822+LoneHilke@users.noreply.github.com>
2023-03-18 00:07:40 +0100

    try to get evrything in
    mangler at kunne få de forskellige kategorier med
    
    Co-Authored-By: Qvisty <18654175+qvisty@users.noreply.github.com>

--- opgave/censor/urls.py
+++ opgave/censor/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
-from .views import Index, Alle
+from .views import Index, Alle, Fag, Dansk, Tysk, Engelsk, Matematik, Klassetrin, Klasse9, Klasse10
 
 urlpatterns = [
     path('',  Index.as_view(), name='index'),
     path('alle/', Alle.as_view(), name='alle'),
+    path('fag/', Fag.as_view(), name='fag'),
+    path('dansk/', Dansk.as_view(), name='dansk'),
+    path('tysk/', Tysk.as_view(), name='tysk'),
+    path('engelsk/', Engelsk.as_view(), name='engelsk'),
+    path('matematik/', Matematik.as_view(), name='matematik'),
+    path('klassetrin/', Klassetrin.as_view(), name='klassetrin'),
+    path('9klasse/', Klasse9.as_view(), name='9klasse'),
+    path('10klasse/', Klasse10.as_view(), name='10klasse'),
 ]
--- opgave/censor/views.py
+++ opgave/censor/views.py
@@ -6,12 +6,12 @@ from .models import Censor, Klassetrin, Fag, Teacher
 # Create your views here.
 class Index(TemplateView):
 
-    template_name = 'censor/base.html'
+    template_name = 'censor/index.html'
 
 class Alle(View):
     def get(self, request, *args, **kwargs):
-        fag = Censor.objects.all ='Fag'
-        klassetrin = Censor.objects.all = 'Klassetrin'
+        fag = Censor.objects.filter(fag__fag__contains ='Fag')
+        klassetrin = Censor.objects.all= 'Klassetrin'
         teacher = Censor.objects.all = 'Teacher'
 
         context= {
@@ -21,3 +21,110 @@ class Alle(View):
         }
         return render(request, 'censor/alle.html', context)
 
+class Fag(View):
+    def get(self, request, *args, **kwargs):
+        fag = Censor.objects.filter(fag__fag__contains ='Fag')
+        dansk = Censor.objects.filter(fag__fag__contains='dansk')
+        tysk = Censor.objects.filter(fag__fag__contains='tysk')
+        engelsk = Censor.objects.filter(fag__fag__contains='engelsk')
+        matematik = Censor.objects.filter(fag__fag__contains='matematik')
+
+        context= {
+            'fag': fag,
+            'dansk': dansk,
+            'tysk': tysk,
+            'matematik': matematik,
+            'engelsk': engelsk
+        }
+        return render(request, 'censor/fag.html', context)
+    
+class Dansk(View):
+    def get(self, request, *args, **kwargs):
+        dansk = Censor.objects.filter(fag__fag__contains='dansk')
+        #klassetrin = Censor.objects.filter( klassetrin__klasse__contains = 'Klassetrin')
+        #teacher = Censor.objects.filter(teacher__teacher__contains = 'Teacher')
+
+        context= {
+            'dansk': dansk,
+            #'klassetrin': klassetrin,
+            #'teacher': teacher
+        }
+        return render(request, 'censor/dansk.html', context)
+
+class Tysk(View):
+    def get(self, request, *args, **kwargs):
+        tysk = Censor.objects.filter(fag__fag__contains='tysk')
+        #klassetrin = Censor.objects.filter( klassetrin__klasse__contains = 'Klassetrin')
+        #teacher = Censor.objects.filter(teacher__teacher__contains = 'Teacher')
+
+        context= {
+            'tysk': tysk,
+            #'klassetrin': klassetrin,
+            #'teacher': teacher
+        }
+        return render(request, 'censor/tysk.html', context)
+    
+class Engelsk(View):
+    def get(self, request, *args, **kwargs):
+        engelsk = Censor.objects.filter(fag__fag__contains='engelsk')
+        #klassetrin = Censor.objects.filter( klassetrin__klasse__contains = 'Klassetrin')
+        #teacher = Censor.objects.filter(teacher__teacher__contains = 'Teacher')
+
+        context= {
+            'engelsk': engelsk,
+            #'klassetrin': klassetrin,
+            #'teacher': teacher
+        }
+        return render(request, 'censor/engelsk.html', context)
+    
+class Matematik(View):
+    def get(self, request, *args, **kwargs):
+        matematik = Censor.objects.filter(fag__fag__contains='matematik')
+        #klassetrin = Censor.objects.filter( klassetrin__klasse__contains = 'Klassetrin')
+        #teacher = Censor.objects.filter(teacher__teacher__contains = 'Teacher')
+
+        context= {
+            'matematik': matematik,
+            #'klassetrin': klassetrin,
+            #'teacher': teacher
+        }
+        return render(request, 'censor/matematik.html', context)
+    
+class Klassetrin(View):
+    def get(self, request, *args, **kwargs):
+        klassetrin = Censor.objects.all='klassetrin'
+        klasse9 = Censor.objects.all='klasse9'
+        klasse10 = Censor.objects.all='klasse10'
+
+        context= {
+            'klassetrin': klassetrin,
+            'klasse9': klasse9,
+           'klasse10': klasse10,
+        }
+        return render(request, 'censor/klassetrin.html', context)
+
+class Klasse9(View):
+    def get(self, request, *args, **kwargs):
+        klasse9 = Censor.objects.all='klasse9'
+        #klassetrin = Censor.objects.filter( klassetrin__klasse__contains = 'Klassetrin')
+        #teacher = Censor.objects.filter(teacher__teacher__contains = 'Teacher')
+
+        context= {
+            'klasse9': klasse9,
+            #'klassetrin': klassetrin,
+            #'teacher': teacher
+        }
+        return render(request, 'censor/9klasse.html', context)
+    
+class Klasse10(View):
+    def get(self, request, *args, **kwargs):
+        klasse10 = Censor.objects.all='klasse10'
+        #klassetrin = Censor.objects.filter( klassetrin__klasse__contains = 'Klassetrin')
+        #teacher = Censor.objects.filter(teacher__teacher__contains = 'Teacher')
+
+        context= {
+            'klasse10': klasse10,
+            #'klassetrin': klassetrin,
+            #'teacher': teacher
+        }
+        return render(request, 'censor/10klasse.html', context)
